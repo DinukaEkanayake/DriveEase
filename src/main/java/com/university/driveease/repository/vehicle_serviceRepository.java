217be1f3b525616d9503f89864d8470812b3f7fe
Dinuka Ekanayake <dinukaekanayaka18@gail.com>
2023-09-21 00:37:23 +0530

    create vehicle_service repository

---
+++ src/main/java/com/university/driveease/repository/vehicle_serviceRepository.java
@@ -0,0 +1,9 @@
+package com.university.driveease.repository;
+
+import com.university.driveease.model.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface vehicle_serviceRepository extends JpaRepository<Vehicle, Long> {
+
+
+}
