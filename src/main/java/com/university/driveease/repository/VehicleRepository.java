5476bcffb7e1be42e8c7b8e4bcff83580b5ecd48
Dinuka Ekanayake <dinukaekanayaka18@gail.com>
2023-09-21 13:11:59 +0530

    rename vehicle repository

--- src/main/java/com/university/driveease/repository/VehicleRepository.java
+++ src/main/java/com/university/driveease/repository/VehicleRepository.java
@@ -2,8 +2,10 @@ package com.university.driveease.repository;
 
 import com.university.driveease.model.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface vehicle_serviceRepository extends JpaRepository<Vehicle, Long> {
+@Repository
+public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
 
 
 }
