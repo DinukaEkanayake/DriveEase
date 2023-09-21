cb9c272431017cf774470abdb407bd1b3afb4421
Dinuka Ekanayake <dinukaekanayaka18@gail.com>
2023-09-21 13:13:05 +0530

    update user profile service

--- src/main/java/com/university/driveease/service/UserProfileService.java
+++ src/main/java/com/university/driveease/service/UserProfileService.java
@@ -1,10 +1,10 @@
 package com.university.driveease.service;
 
 import com.university.driveease.dto.ResponseDTO;
-import com.university.driveease.model.User;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
-
+@Service
 public interface UserProfileService {
 
     ResponseDTO getUser(Model model,OidcUser principal);
