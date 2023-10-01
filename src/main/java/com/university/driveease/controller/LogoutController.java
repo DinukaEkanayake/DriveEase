dbd3e568724e99a2f5d3633e461bdfd2547db561
Dinuka Ekanayake <dinukaekanayaka18@gail.com>
2023-10-01 14:18:49 +0530

    create logout controller

---
+++ src/main/java/com/university/driveease/controller/LogoutController.java
@@ -0,0 +1,18 @@
+//package com.university.driveease.controller;
+//
+//import org.springframework.security.core.context.SecurityContextHolder;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class LogoutController {
+//
+////    @GetMapping("/logout")
+////    public String logout() {
+////        // Perform any additional logout actions if needed
+////        SecurityContextHolder.clearContext();
+////        return "redirect:/"; // Redirect to the home page after logout
+////    }
+//}
+//
