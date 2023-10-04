69fe641b4bd96fb9209661dc90af10c75ebb89eb
Dinuka Ekanayake <dinukaekanayaka18@gail.com>
2023-10-04 09:42:53 +0530

    create userprofiledisplayservice

--- src/main/java/com/university/driveease/controller/UserController.java
+++ src/main/java/com/university/driveease/controller/UserController.java
@@ -16,26 +16,9 @@ import java.util.LinkedHashMap;
 public class UserController {
 
 	@GetMapping("/")
-	public String handleOAuth2Callback(@AuthenticationPrincipal OAuth2User oauth2User, Model model) throws JsonProcessingException {
+	public String displayHomePage(@AuthenticationPrincipal OAuth2User oauth2User, Model model) {
 
-		if (oauth2User != null){
-			String username = oauth2User.getAttribute("username");
-			String name = oauth2User.getAttribute("given_name");
-			String email = oauth2User.getAttribute("email");
-			String contactNo = oauth2User.getAttribute("phone_number");
-
-			// Add user details to the model
-			model.addAttribute("oauth2User",oauth2User);
-			model.addAttribute("username", username);
-			model.addAttribute("name", name);
-			model.addAttribute("email", email);
-			model.addAttribute("contactNo", contactNo);
-
-			LinkedHashMap<String, Object> address = oauth2User.getAttribute("address");
-			String country = (String) address.get("country");
-			model.addAttribute("country", country);
-		}
-
+		model.addAttribute("oauth2User",oauth2User);
 		return "home";
 	}
 
--- src/main/java/com/university/driveease/controller/UserProfileController.java
+++ src/main/java/com/university/driveease/controller/UserProfileController.java
@@ -1,28 +1,36 @@
 package com.university.driveease.controller;
 
-import com.university.driveease.dto.ResponseDTO;
-import com.university.driveease.service.UserProfileService;
-import lombok.AllArgsConstructor;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-//@RequestMapping(value = "api/")
-@AllArgsConstructor
+import java.util.LinkedHashMap;
+
+@Controller
 public class UserProfileController {
 
-    private final UserProfileService userProfileService;
-
     @GetMapping("/profile")
-    public String getUserProfile(OAuth2AuthenticationToken token) {
+    public String displayUserProfile(@AuthenticationPrincipal OAuth2User oauth2User, Model model){
 
-        ResponseDTO user=userProfileService.getUser(token);
+            String username = oauth2User.getAttribute("username");
+            String name = oauth2User.getAttribute("given_name");
+            String email = oauth2User.getAttribute("email");
+            String contactNo = oauth2User.getAttribute("phone_number");
 
-        //Display user information in the view
-//        return "Name: " + user.getName() + "<br>Email: " + user.getEmail() +
-//                "<br>Contact Number: " + user.getContactNumber() + "<br>Country: " + user.getCountry();
+            // Add user details to the model
+            model.addAttribute("username", username);
+            model.addAttribute("name", name);
+            model.addAttribute("email", email);
+            model.addAttribute("contactNo", contactNo);
 
-        return "home";
+            LinkedHashMap<String, Object> address = oauth2User.getAttribute("address");
+            String country = (String) address.get("country");
+            model.addAttribute("country", country);
+
+        return "profile";
     }
 
 
