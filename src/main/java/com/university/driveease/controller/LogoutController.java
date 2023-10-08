27c0f4dc9a836f175abca09b44207a0e404a0733
Dinuka Ekanayake <dinukaekanayaka18@gail.com>
2023-10-08 23:20:14 +0530

    update logout to setAuthentication details

--- src/main/java/com/university/driveease/controller/LogoutController.java
+++ src/main/java/com/university/driveease/controller/LogoutController.java
@@ -1,36 +1,18 @@
 package com.university.driveease.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 @Controller
 public class LogoutController {
     @GetMapping("/logout")
-    public String logout(HttpServletRequest request, HttpServletResponse response) {
-        SecurityContextHolder.clearContext();
-        // Invalidate the user's session (if applicable)
-        HttpSession session = request.getSession(false);
-        if (session != null) {
-            session.invalidate();
-        }
-
-        // Remove authentication cookies (if applicable)
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if ("cookieName".equals(cookie.getName())) {
-                    // Set the cookie's max age to 0 to delete it
-                    cookie.setMaxAge(0);
-                    cookie.setPath("/");
-                    response.addCookie(cookie);
-                }
-            }
+    public String logout() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            SecurityContextHolder.getContext().setAuthentication(null);
         }
 
         return "index";
