4fc908b467f8a9e256d12d5d6bbe23ecb2733b70
Dinuka Ekanayake <dinukaekanayaka18@gail.com>
2023-10-08 23:21:44 +0530

    update VehicleRepository.java

--- src/main/java/com/university/driveease/repository/VehicleRepository.java
+++ src/main/java/com/university/driveease/repository/VehicleRepository.java
@@ -2,22 +2,14 @@ package com.university.driveease.repository;
 
 import com.university.driveease.model.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
 
     List<Vehicle> findAllByUsername(String username);
-
-    @Query(value = "SELECT v FROM Vehicle v WHERE v.vehicle_no = ?1")
-    Optional<Vehicle> findByVehicle_no(String vehicle_no);
-//    PreparedStatement preparedStatement = connection.prepareStatement("SELECT v FROM Vehicle v WHERE v.vehicle_no = ?");
-//    preparedStatement.setString(1, userInput);
-//    ResultSet resultSet = preparedStatement.executeQuery();
 
 }
